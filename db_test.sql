849e1b2ff470d119fbeb2a4a666cb6d8a59cd70d
spitsyn <spitsyn@lanit.ru>
2019-10-01 22:19:00 +0500

    sql add

--- db_test.sql
+++ db_test.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `peoples` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) DEFAULT NULL,
-  `middlename` varchar(255) DEFAULT NULL,
-  `lastname` varchar(255) DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `middlename` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
